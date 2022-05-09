cd56de0cb1ed96bb9c5d6bc6ed41c6ba776bdf53
Jason hanks <jason.e.hanks@gmail.com>
2022-05-09 11:57:49 -0500

    Changed verbage for test case contexts

--- frontend/cypress/integration/01_security/login_user.spec.ts
+++ frontend/cypress/integration/01_security/login_user.spec.ts
@@ -1,7 +1,6 @@
 
 describe('Validate Login form behavior', () => {
 
-
   beforeEach(() => {
     cy.visit('/')
     cy.intercept('POST', '/api/users/login', {fixture: 'api/users/login-invalid.json'}).as('invalidLogin')
@@ -11,43 +10,41 @@ describe('Validate Login form behavior', () => {
 
   context('Client side form validations', () => {
 
-
-    it('should show form errors by default', () => {
+    it('shows form errors by default', () => {
       cy.get('#login').should('be.visible')
       cy.get('#field-1-helptext').should("have.text", "Email address is required as your login.")
       cy.get('#field-2-feedback').should("have.text", "Enter a valid password.")
     })
 
-
-    it('should change Password help text when filled out', () => {
+    it('changes Password help text when filled out', () => {
+      cy.get('#field-1-helptext').should("have.text", "Email address is required as your login.")
+      cy.get('#field-2-feedback').should("have.text", "Enter a valid password.")
       cy.get('#password').click().type('asdfasdf')
       cy.get('#field-2-helptext').should("have.text", "Never reuse or share your passwords with anyone.")
     })
 
-
-    it('should show no errors when properly filled out', () => {
+    it('shows no errors when properly filled out', () => {
+      cy.get('#field-1-helptext').should("have.text", "Email address is required as your login.")
+      cy.get('#field-2-feedback').should("have.text", "Enter a valid password.")
       cy.get('#username').click().type('valid-user@gmail.com')
       cy.get('#password').click().type('asdfasdf')
       cy.get('#field-1-helptext').should("not.exist")
       cy.get('#field-2-helptext').should("have.text", "Never reuse or share your passwords with anyone.")
     })
 
-
   })
 
 
-  context('API - login error validations', () => {
+  context('API - invalid login validations', () => {
 
-
-    it('should show error when using an empty username', () => {
+    it('shows error when using an empty username', () => {
       cy.get('#password').click().type('asdfasdf')
       cy.get('#login').click()
       cy.wait('@invalidLogin')
       cy.get('#errors').should("have.text", "Invalid username or password")
     })
   
-  
-    it('should show error when using username is not an email address', () => {
+    it('shows error when using username is not an email address', () => {
       cy.get('#username').click().type('not-an-email-address')
       cy.get('#password').click().type('asdfasdf')
       cy.get('#login').click()
@@ -55,34 +52,29 @@ describe('Validate Login form behavior', () => {
       cy.get('#errors').should("have.text", "Invalid username or password")
     })
   
-  
-    it('should show error when using an empty password', () => {
+    it('shows error when using an empty password', () => {
       cy.get('#username').click().type('valid-user@gmail.com')
       cy.get('#login').click()
       cy.wait('@invalidLogin')
       cy.get('#errors').should("have.text", "Invalid username or password")
     })
   
-
-    it('should show error when Submitting an empty form', () => {
+    it('shows error when Submitting an empty form', () => {
       cy.get('#login').click()
       cy.wait('@invalidLogin')
       cy.get('#errors').should("have.text", "Invalid username or password")
     })
-  
 
   })
 
 
-  context('API - login success validations', () => {
-
+  context('API - successful login validations', () => {
 
     beforeEach(() => {
       cy.intercept('POST', '/api/users/login', {fixture: 'api/users/login-valid.json' }).as('validLogin')
     })
 
-
-    it('should navigate the user to the Dashboard with valid credentials', () => {
+    it('navigates the user to the Dashboard with valid credentials', () => {
       cy.get('#username').click().type('valid-user@gmail.com')
       cy.get('#password').click().type('asdfasdf')
       cy.get('#field-1-helptext').should("not.exist")
@@ -109,5 +101,6 @@ describe('Validate Login form behavior', () => {
     })
 
   })
+
 
 })
