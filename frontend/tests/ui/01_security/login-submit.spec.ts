96a8394d512e5fdeda49333e5c31f83e26c94cab
Jason hanks <jason.e.hanks@gmail.com>
2022-05-20 16:24:46 -0500

    Split the login spec test to run in parallel

--- frontend/tests/ui/01_security/login-submit.spec.ts
+++ frontend/tests/ui/01_security/login-submit.spec.ts
@@ -9,42 +9,15 @@ const TEXT_PASSWD_REUSE   = 'Never reuse or share your passwords with anyone.'
 const TEXT_VALID_PASSWD   = 'Enter a valid password.'
 
 
-test.describe('Login form', () => {
+test.describe('Login form submissions', () => {
 
   test.beforeEach(async ({ page }) => {
     await page.goto('/')
-    await page.click("text=/Accept/") //Close the disclaimer for Firefox
+    await page.click("text=/Accept/") // Close the disclaimer for Firefox
 
     // Network intercept routes
     await page.route('**/api/users/login', async (route) => { await route.fulfill({ status: 401, body: '{}' })})
     await page.route('**/api/projects', async (route) => { await route.fulfill({ status: 200, body: JSON.stringify(defaultProject) }) })
-  })
-
-
-  test.describe('Form validations', () => {
-
-    test('show errors by default', async ({ page }) => {
-      await expect(page.locator('#submit')).toBeVisible()
-      await expect(page.locator('#field-1-helptext')).toHaveText(TEXT_EMAIL_REQD)
-      await expect(page.locator('#field-2-feedback')).toHaveText(TEXT_VALID_PASSWD)
-    })
-
-    test('changes Password help text when filled out', async ({ page }) => {
-      await expect(page.locator('#field-1-helptext')).toHaveText(TEXT_EMAIL_REQD)
-      await expect(page.locator('#field-2-feedback')).toHaveText(TEXT_VALID_PASSWD)
-      await page.fill('#password', 'asdfasdf')
-      await expect(page.locator('#field-2-helptext')).toHaveText(TEXT_PASSWD_REUSE)
-    })
-
-    test('show no errors when properly filled out', async ({ page }) => {
-      await expect(page.locator('#field-1-helptext')).toHaveText(TEXT_EMAIL_REQD)
-      await expect(page.locator('#field-2-feedback')).toHaveText(TEXT_VALID_PASSWD)
-      await page.fill('#username', 'valid-user@gmail.com')
-      await page.fill('#password', 'asdfasdf')
-      await expect(page.locator("#field-1-helptext")).toHaveText(TEXT_EMAIL_REQD)
-      await expect(page.locator('#field-2-helptext')).toHaveText(TEXT_PASSWD_REUSE)
-    })
-
   })
 
 
